6e641145c326ee1c0fabda8cd8a1003082ce744c
Jyotishman Paul <dontsentmemails@yahoo.com>
2016-01-21 05:42:02 +0530

    First commit , a long way to go

---
+++ app.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    var taxiTest = angular.module('taxiTest', ['firebase', 'ngMaterial', 'angular-md5', 'ui.router']);
+    
+    taxiTest.config(function($stateProvider,$urlRouterProvider){
+        
+        $stateProvider
+        
+                .state('login',{
+                    url:'/login' ,
+                    templateUrl : 'components/auth/authView.html'
+                });
+                
+        
+    });
+    
+})();
