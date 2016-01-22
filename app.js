fc9cf326d2149772a542bd3a588c79dc1715c1a7
Jyotishman Paul <dontsentmemails@yahoo.com>
2016-01-22 23:49:08 +0530

    Minor change : Home View added

--- app.js
+++ app.js
@@ -2,17 +2,22 @@
     'use strict';
 
     var taxiTest = angular.module('taxiTest', ['firebase', 'ngMaterial', 'angular-md5', 'ui.router']);
-    
-    taxiTest.config(function($stateProvider,$urlRouterProvider){
-        
+
+    taxiTest.config(function ($stateProvider, $urlRouterProvider) {
+
         $stateProvider
-        
-                .state('login',{
-                    url:'/login' ,
-                    templateUrl : 'components/auth/authView.html'
+
+                .state('login', {
+                    url: '/login',
+                    templateUrl: 'components/auth/authView.html'
+                })
+
+                .state('home', {
+                    url: '/home',
+                    templateUrl :'components/home.html'
                 });
-                
-        
+
+
     });
-    
+
 })();
