d7dca3bfbd2d222c662639c954e78269b79eba74
Jyotishman Paul <dontsentmemails@yahoo.com>
2016-02-04 21:57:00 +0530

    Auth for Firebase

--- components/auth/authService.js
+++ components/auth/authService.js
@@ -1,1 +1,14 @@
+(function() {
 
+  'use strict';
+
+  angular
+    .module('taxiTest')
+    .factory('Auth', AuthService);
+
+  function AuthService($firebaseAuth) {
+    var ref = new Firebase("https://taxitest.firebaseio.com");
+    return $firebaseAuth(ref);
+  }
+
+})();
